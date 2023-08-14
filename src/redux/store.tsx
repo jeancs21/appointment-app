3916ca7783267f905f31f37b218791c26393d7fa
jeancs21 <40588173+jeancs21@users.noreply.github.com>
2023-08-14 12:34:02 -0400

    persist patient data. initialize app with empty patiente data. Persist updated data. Remove reset function from edit patient form. This last change due to reseting data after updating it.

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -3,15 +3,21 @@ import { PatientFormValues } from "../model/patient.model";
 import { patientSlice } from "./states/patient.state";
 import { AppointmentFormValues } from "../model/appointment.model";
 import { appointmentSlice } from "./states/appointment.state";
+import { loadDbPatientState } from "../services/persist-data/patients/persist-patient-info.services";
 
 export interface AppStore {
     patient: PatientFormValues[];
     appointment: AppointmentFormValues[];
 }
 
+const initialState = {
+    patient: loadDbPatientState() || []
+}
+
 export default configureStore<AppStore>({
     reducer: {
         patient: patientSlice.reducer,
         appointment: appointmentSlice.reducer
-    }
+    },
+    preloadedState: initialState
 })

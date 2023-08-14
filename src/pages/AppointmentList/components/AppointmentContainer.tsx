00cfc52e7366c10612890a0d3705c76000dd1de8
jeancs21 <40588173+jeancs21@users.noreply.github.com>
2023-08-14 16:34:51 -0400

    Change some styling to appointment list

--- src/pages/AppointmentList/components/AppointmentContainer.tsx
+++ src/pages/AppointmentList/components/AppointmentContainer.tsx
@@ -4,6 +4,7 @@ import { PublicRoutes } from "../../../model/routes";
 import NavigateButton from "../../../containers/Buttons/NavigateButton";
 import { Link } from "react-router-dom";
 import { PencilSquareIcon, TrashIcon } from "@heroicons/react/20/solid";
+import { AppointmentStatusEnum } from "../../../model/enums/appointmentStatus.enum";
 
 type Props = {
     appointments: AppointmentFormValues[];
@@ -12,7 +13,7 @@ type Props = {
 const AppointmentContainer:FunctionComponent<Props> = (props) => {
   return (
     <>
-        <div className="flex w-full justify-end mb-6">
+        <div className="flex container justify-end mb-6">
             <NavigateButton path={PublicRoutes.ADD_APPOINTMENT} buttonText="Agendar cita" />
         </div>
         <div className='flex flex-col justify-center container w-full'>
@@ -21,16 +22,16 @@ const AppointmentContainer:FunctionComponent<Props> = (props) => {
                     return (
                         <div
                             key={appointment.id}
-                            className="flex justify-evenly gap-24 bg-pink-400 p-4 m-4 rounded-md drop-shadow-xl hover:scale-105 duration-300 cursor-pointer items-center"
+                            className="self-center flex flex-col sm:flex-row sm:justify-evenly gap-12 sm:gap-24 w-64 sm:w-full bg-slate-100 p-4 m-4 border border-pink-500 rounded-md drop-shadow-xl hover:scale-105 duration-300 cursor-pointer items-center"
                         >
-                            <p className="text-white">{appointment.patient}</p>
-                            <p className="text-white">{appointment.appointmentDate}</p>
-                            <p className="text-white">{appointment.status}</p>
+                            <p className="text-pink-600">{appointment.patient}</p>
+                            <p className="text-pink-600">{appointment.appointmentDate}</p>
+                            <p className={`p-1 rounded-xl text-white w-24 text-center ${appointment.status === AppointmentStatusEnum.Cancelado ? 'bg-red-500' : 'bg-green-500'}`}>{appointment.status}</p>
                             <div className='flex gap-8'>
                                 <Link to={`/edit-appointment/${appointment.id}`}>
-                                    <PencilSquareIcon className='w-8 h-8 m-0 fill-white cursor-pointer hover:fill-slate-300 duration-300'/>
+                                    <PencilSquareIcon className='w-8 h-8 m-0 fill-pink-500 cursor-pointer hover:fill-pink-300 duration-300'/>
                                 </Link>
-                                <TrashIcon className='w-8 h-8 m-0 fill-white cursor-pointer hover:fill-slate-300 duration-300' />
+                                <TrashIcon className='w-8 h-8 m-0 fill-pink-500 cursor-pointer hover:fill-pink-300 duration-300' />
                             </div>
                         </div>
                     )
--- src/pages/EditAppointment/components/CancelAppointment.tsx
+++ src/pages/EditAppointment/components/CancelAppointment.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react'
+import { FunctionComponent } from 'react'
 import { AppointmentStatusEnum } from '../../../model/enums/appointmentStatus.enum'
 
 type Props = {
